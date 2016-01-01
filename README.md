759035c39a8199f130ec607c157226cf6254c25b
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2016-01-02 02:31:24 +0530

    Updated Package with BSD

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 pacman-game
------------
-A pacman game built entirely in python.
+===============================
 
-Build Badges
-------------
-
+version number: 1.0.1.dev1
+author: Parth Kolekar
 [![Build Status](https://travis-ci.org/ParthKolekar/pacman-game.svg?branch=master)](https://travis-ci.org/ParthKolekar/pacman)
 [![Coverage Status](https://coveralls.io/repos/ParthKolekar/pacman-game/badge.svg)](https://coveralls.io/r/ParthKolekar/pacman)
 [![PyPI version](https://badge.fury.io/py/pacman-game.svg)](https://pypi.python.org/pypi/pacman-game/)
-[![License](https://img.shields.io/badge/license-GPL-blue.svg)](https://pypi.python.org/pypi/pacman-game/)
 
+Overview
+--------
+
+A Pacman Like Game with new and interesting new features.
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install pacman-game
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/ParthKolekar/pacman-game.git
+    $ python setup.py install
+    
--- setup.py
+++ setup.py
@@ -1,26 +1,45 @@
-"""
-    A simple python based game
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-    Contains some basic AI and Randomly generated Maps
-    ANSI Colors are added for pretty looks
-"""
+VERSION = '1.0.1.dev1'
 
-from setuptools import setup
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
 
 setup(
     name='pacman-game',
-    version='1.0.1.dev1',
-    description='A simple Pacman Game',
-    long_description=__doc__,
-    license='LGPLv3',
-    author='Parth Laxmikant Kolekar',
-    author_email='parth.kolekar@students.iiit.ac.in',
-    packages=['pacman_game'],
+    version=VERSION,
+    description='A Pacman Like Game with new and interesting new features.',
+    long_description=long_description,
     url='https://github.com/ParthKolekar/pacman-game',
+    download_url='https://github.com/ParthKolekar/pacman-game/tarball/' + VERSION,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='pacman console ai game',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Parth Kolekar',
+    install_requires=install_requires,
+    depedency_links=dependency_links,
+    author_email='parth.kolekar@students.iiit.ac.in',
     entry_points={
         'console_scripts': [
-            'pacman-game = pacman_game.main:main',
-        ]
+            'pacman-game=pacman_game.main:main',
+        ],
     },
-    keywords='pacman console ai simple basic',
 )
